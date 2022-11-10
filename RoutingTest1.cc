1a34dcc008340c9ec4f0c7ed0577cf1585791d25
MohamadDalal <71063283+MohamadDalal@users.noreply.github.com>
2022-11-10 14:19:51 +0100

    Added congestion window tracing
    The script now traces and logs the congestion window of Node 0. The TCP socket being used is Westwood.

--- RoutingTest1.cc
+++ RoutingTest1.cc
@@ -11,11 +11,62 @@ using namespace ns3;
 
 NS_LOG_COMPONENT_DEFINE ("RoutingTest");
 
+static std::map<uint32_t, bool> firstCwnd;
+static std::map<uint32_t, Ptr<OutputStreamWrapper>> cWndStream;
+static std::map<uint32_t, uint32_t> cWndValue;
+
+static uint32_t
+GetNodeIdFromContext (std::string context)
+{
+  std::size_t const n1 = context.find_first_of ("/", 1);
+  std::size_t const n2 = context.find_first_of ("/", n1 + 1);
+  return std::stoul (context.substr (n1 + 1, n2 - n1 - 1));
+}
+
+static void
+CwndTracer (std::string context,  uint32_t oldval, uint32_t newval)
+{
+  NS_LOG_LOGIC("Called CwndTracer");
+  uint32_t nodeId = GetNodeIdFromContext (context);
+
+  if (firstCwnd[nodeId])
+    {
+      *cWndStream[nodeId]->GetStream () << "0.0 " << oldval << std::endl;
+      firstCwnd[nodeId] = false;
+    }
+  *cWndStream[nodeId]->GetStream () << Simulator::Now ().GetSeconds () << " " << newval << std::endl;
+  cWndValue[nodeId] = newval;
+  NS_LOG_LOGIC("Finished CwndTracer");
+}
+
+static void
+TraceCwnd (std::string cwnd_tr_file_name, uint32_t nodeId)
+{
+  NS_LOG_LOGIC("Called TraceCwnd");
+  AsciiTraceHelper ascii;
+  cWndStream[nodeId] = ascii.CreateFileStream (cwnd_tr_file_name.c_str ());
+  Config::Connect ("/NodeList/" + std::to_string (nodeId) + "/$ns3::TcpL4Protocol/SocketList/0/CongestionWindow",
+                   MakeCallback (&CwndTracer));
+  NS_LOG_LOGIC("Finished TraceCwnd");
+}
+
+
 int main(int argc, char *argv[])
 {
-    CommandLine cmd (__FILE__);
-    cmd.Parse (argc, argv);
+    std::string transport_prot = "TcpWestwood";
 
+    CommandLine cmd (__FILE__);
+    cmd.AddValue ("transport_prot", "Transport protocol to use: TcpNewReno, TcpLinuxReno, "
+                "TcpHybla, TcpHighSpeed, TcpHtcp, TcpVegas, TcpScalable, TcpVeno, "
+                "TcpBic, TcpYeah, TcpIllinois, TcpWestwood, TcpWestwoodPlus, TcpLedbat, "
+		        "TcpLp, TcpDctcp, TcpCubic, TcpBbr", transport_prot);
+    cmd.Parse (argc, argv);
+    transport_prot = std::string ("ns3::") + transport_prot;
+    TypeId tcpTid;
+    NS_ABORT_MSG_UNLESS (TypeId::LookupByNameFailSafe (transport_prot, &tcpTid), "TypeId " << transport_prot << " not found");
+    Config::SetDefault ("ns3::TcpL4Protocol::SocketType", TypeIdValue (TypeId::LookupByName (transport_prot)));
+
+    LogComponentEnable ("RoutingTest", LOG_LEVEL_ALL);
     LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
     //LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
     LogComponentEnable ("OnOffApplication", LOG_LEVEL_INFO);
@@ -24,7 +75,7 @@ int main(int argc, char *argv[])
     //LogComponentEnable ("PacketSink", LOG_LEVEL_ALL);
     Time::SetResolution (Time::NS);
     Config::SetDefault ("ns3::OnOffApplication::PacketSize", UintegerValue (250));
-    Config::SetDefault ("ns3::OnOffApplication::DataRate", StringValue ("5kb/s"));
+    Config::SetDefault ("ns3::OnOffApplication::DataRate", StringValue ("50kb/s"));
 
     NS_LOG_INFO("Creating Topology");
     NodeContainer nodes;
@@ -123,6 +174,8 @@ int main(int argc, char *argv[])
     AsciiTraceHelper ascii;
     pointToPoint.EnableAsciiAll (ascii.CreateFileStream ("scratch/RoutingTest1.tr"));
     pointToPoint.EnablePcapAll ("scratch/RoutingTest1");
+    firstCwnd[1] = true;
+    Simulator::Schedule (Seconds (1.00001), &TraceCwnd, "scratch/RoutingTest1-cwnd.data", 0);
     AnimationInterface anim("RoutingTest1.xml");
     Simulator::Run ();
     Simulator::Stop(Seconds(10.0));
